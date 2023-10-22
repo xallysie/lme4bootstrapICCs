d84805fec8744adb971432afdaef510258676d54
Sally Xie <48843867+xallysie@users.noreply.github.com>
2023-10-21 20:14:37 -0400

    Update ICC_bootstrapscript.R
    # updated 11/17/2021 - the latest update (01/05/2021) to the lmeresampler package basically makes this function obsolete, hooray!
    #                    - it is now a convenience function to make the output easier to read

--- ICC_bootstrapscript.R
+++ ICC_bootstrapscript.R
@@ -1,6 +1,8 @@
 # Bootstrapping Confidence Intervals around ICCs [osf.io/anwx2/]
 # Updated 17/01/2020 for lme4 version 1.1-21 and boot version 1.3-24
 # updated 12/02/2020 to be able to accommodate any number of clusters and their interactions
+# updated 11/17/2021 - the latest update (01/05/2021) to the lmeresampler package basically makes this function obsolete, hooray!
+#                    - it is now a convenience function to make the output easier to read
 
 # library
 library(boot)     # bootstrapping
@@ -10,12 +12,13 @@ library(lmeresampler) # resampling mixed-effects models
 library(tidyr)    # data hygiene
 
 ## uncomment to install the lmeresampler package from github
-#if(!require(devtools)) install.packages("devtools")
-#devtools::install_github("aloy/lmeresampler")
+## if(!require(devtools)) install.packages("devtools",dependencies=TRUE, repos='http://cran.rstudio.com/')
+## devtools::install_github("aloy/lmeresampler")
 
 # import sample dataset 
 
-df <- read.csv("//FILEPATH/sample_df.csv")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+df <- read.csv("sample_df.csv")
 df <- df[df$WhatWasRating=="agg",] # look at ratings of aggressiveness
 
 # This script will estimate intraclass correlation coefficients (ICC) for each cluster in a model with k clusters. 
@@ -48,7 +51,7 @@ bootstrapICC <- function(model,             # Lmer model object
                          boot.type,         # A character string indicating the type of bootstrap that is being requested,
                          ## e.g., "parametric", "residual", "case", "cgr", "reb" (see ?lmeresampler::bootstrap for more info)
                          boot.ci.type,      # Enter a character string indicating the type of interval that is being requested,
-                         ## e.g., "norm", "basic", "stud", "perc", "bca" (see ?boot.ci for more info)
+                         ## e.g., "all", "norm", "basic", "stud", "perc", "bca" (see ?lmeresampler::confint for more info)
                          ...){             
   m <- model
   ranefs = names(getME(m,"cnms")) # get cluster names
@@ -76,34 +79,39 @@ bootstrapICC <- function(model,             # Lmer model object
   pointsummary = mysumm(m)
   
   # bootstrap your lmer object
-  bootie <- lmeresampler::bootstrap(model = m, fn=mysumm, type=boot.type, B=iterations)
+  bootie <- lmeresampler::bootstrap(model = m, .f=mysumm, type=boot.type, B=iterations)
   
   # store bootstrapped samples in dataframe
-  dfbootie <- as.data.frame(bootie[['t']])
-  names(dfbootie) <- names(pointsummary)
-  
+  #dfbootie <- as.data.frame(bootie[['replicates']])
+  #names(dfbootie) <- names(pointsummary)
+
   # what to extract from boot.ci (don't touch this unless boot.ci got updated and broke things)
-  boot.ci.index <- as.numeric(c(1:(2*ranef_k+3)))
+  #boot.ci.index <- as.numeric(c(1:(2*ranef_k+3)))
+  CIs <- confint(bootie, level = 0.95, type = boot.ci.type)
   
   # store 95% CIs
-  CIs     <- lapply(boot.ci.index, function(i){boot.ci(bootie, index=i, type=boot.ci.type)})
-  CIs.out <- sapply(CIs, function(j){tail(as.numeric(j[[4]]),2)}) 
-  CIs.out <- as.data.frame(t(CIs.out))
-  names(CIs.out) <- c("l-95% CI", "u-95% CI")
-  row.names(CIs.out) <- c(names(pointsummary[1:length(pointsummary)]))
-  pointsummary <- as.data.frame(pointsummary); names(pointsummary) = "Estimate"
-  CIs.out <- cbind(CIs.out, pointsummary)
+  #CIs     <- lapply(boot.ci.index, function(i){boot.ci(bootie, index=i, type=boot.ci.type)})
+  #CIs.out <- sapply(CIs, function(j){tail(as.numeric(j[[4]]),2)}) 
+  #CIs.out <- as.data.frame(t(CIs.out))
+  #names(CIs.out) <- c("l-95% CI", "u-95% CI")
+  #row.names(CIs.out) <- c(names(pointsummary[1:length(pointsummary)]))
+  #pointsummary <- as.data.frame(pointsummary); names(pointsummary) = "Estimate"
+  #CIs.out <- cbind(CIs.out, pointsummary)
   
   # split variances and ICCs into diff columns
-  CIs.ICC <- CIs.out[grepl("ICC", rownames(CIs.out))==TRUE,]
-  CIs.var <- CIs.out[grepl("ICC", rownames(CIs.out))==FALSE,]
+  #CIs.ICC <- CIs.out[grepl("ICC", rownames(CIs.out))==TRUE,]
+  #CIs.var <- CIs.out[grepl("ICC", rownames(CIs.out))==FALSE,]
   
   # print CI output
-  print(CIs.var,digits=5); print(CIs.ICC,digits=5)
+  #print(CIs.var,digits=5); print(CIs.ICC,digits=5)
   
   # return list containing boot object, boot.ci output, and the table of 95% CIs with their Point Estimates
-  merp <- list(bootie, dfbootie, CIs, CIs.var, CIs.ICC)
-  names(merp) <- c("boot_object", "df_bootsamples", "bootCI_object", "CI_table_Variance", "CI_table_ICC")
+  #merp <- list(bootie, dfbootie, CIs, CIs.var, CIs.ICC)
+  #names(merp) <- c("boot_object", "df_bootsamples", "bootCI_object", "CI_table_Variance", "CI_table_ICC")
+  
+  print(CIs, digits=5)
+  merp = list(bootie,CIs)
+  names(merp) = c("boot_object","CIs")
   print("Done! Have a great day.")
   return(merp)
 }
@@ -123,9 +131,6 @@ bootstrapICC <- function(model,             # Lmer model object
 booty <- bootstrapICC(model, iterations=100, boot.type="parametric", boot.ci.type="norm")
 
 # The output is a list containing:
-booty[["boot_object"]]       # the original boot object from lmeresampler::bootstrap()
-booty[["df_bootsamples"]]    # a dataframe containing bootstrapped samples
-booty[["bootCI_object"]]     # the boot.ci object
-booty[["CI_table_Variance"]] # a dataframe of 95% Interval Estimates of Variance Components 
-booty[["CI_table_ICC"]]      # a dataframe of 95% Interval Estimates of intraclass correlation coefficients 
+View(booty[["boot_object"]]) # the original boot object from lmeresampler::bootstrap()
+booty[["CIs"]]               # dataframe of 95% Interval Estimates of Variance Components and intraclass correlation coefficients 
  
